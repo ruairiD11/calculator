a08c2195efceaf7705dbbd0a662deb9e69d62f54
RUAIRI DOHERTY - STUDENT <G00352092@gmit.ie>
2020-01-30 11:34:30 +0000

    First commit in my Calculator repository

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,13 @@
+package ie.gmit;
+
+public class Calculator {
+    private int firstNum;
+    private int secondNum;
+
+    public Calculator() {}
+
+    public int add(int firstNum, int secondNum) {
+        int sum = firstNum + secondNum;
+        return sum;
+    }
+}
